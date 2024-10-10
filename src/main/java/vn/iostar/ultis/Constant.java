fdff30beb901d44d5404b94a62c0b79c37a9f769
Hung <vanhung211104@gmail.com>
2024-10-10 22:17:52 +0700

    JPA Servlet CRUD

---
+++ src/main/java/vn/iostar/ultis/Constant.java
@@ -0,0 +1,10 @@
+package vn.iostar.ultis;
+
+public class Constant {
+    public static final String SESSION_USERNAME = "username";
+    public static final String COOKIE_REMEMBER =  "username";
+    public static final String REGISTER = "/views/Register.jsp";
+    public static final String ForgotPassword = "/views/ForgotPassword.jsp";
+    public static final String DIR =  "D:\\UTE\\K1N3\\Images\\";
+    public static final String DEFAULT_FILENAME = "default.file";
+}
